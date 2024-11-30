5ae5f9e397d2a6ba391825a99029bad66c3ee092
ewqc <guaine85@gmail.com>
2024-11-30 19:30:13 -0300

    tp_lab4_v1.9

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -7,14 +7,29 @@ const nombre = document.querySelector('#nombre');
 const apellido = document.querySelector('#apellido');
 const dni = document.querySelector('#dni');
 const telefono = document.querySelector('#telefono');
+const direccion = document.querySelector('#domicilio');
 
 nombre.addEventListener('input', valida);
 apellido.addEventListener('input', valida);
 dni.addEventListener('input', valida);
 telefono.addEventListener('input', valida);
+direccion.addEventListener('input', mensaje);
 
 function valida(evento){
     const validaCadena = new ValidaCadena(evento.target.id, evento.target.value);
+    mensaje(evento);
+}
+
+function mensaje(evento){
+    const campo = evento.target;
+    const validaCampo = campo.validity;
+
+    if(validaCampo.tooShort){
+        campo.setCustomValidity(`El campo ${evento.target.id.toUpperCase()} requiere ${evento.target.minLength} caracteres como minimo`);
+    }else{
+        campo.setCustomValidity("");
+    }
+    campo.reportValidity();
 }
 
 
