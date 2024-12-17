d95595df6d0678d051dfccf1994b72531b79e48b
ewqc <guaine86@gmail.com>
2024-12-16 21:58:03 -0300

    login_v0.9

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,29 @@ const miModulo = (()=>{
     const dni = document.querySelector('#dni');
     const telefono = document.querySelector('#telefono');
     const direccion = document.querySelector('#domicilio');
+    const formulario = document.querySelector('.formulario')
     
     nombre.addEventListener('input', valida);
     apellido.addEventListener('input', valida);
     dni.addEventListener('input', valida);
     telefono.addEventListener('input', valida);
     direccion.addEventListener('input', mensaje);
+    formulario.addEventListener('submit', (evento)=>{
+        const datos = Object.fromEntries(
+            new FormData(evento.target)
+        )
+        //console.log(datos)
+        if(!datos.nombre || !datos.apellido || !datos.dni || !datos.fecha_nac || !datos.telefono || !datos.email || !datos.domicilio || !datos.carrera){
+            evento.preventDefault();
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Debe completar todos los campos!",
+            });
+        }else{
+            return
+        }
+    })
 })();
 
 
