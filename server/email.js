e82a29d95cbb5fda438bd00dc25d81db59b83579
Edward Wayne Cruz <117868589+guaine86@users.noreply.github.com>
2026-03-12 12:30:07 -0300

    Update email.js

--- server/email.js
+++ server/email.js
@@ -2,6 +2,10 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth:{
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
