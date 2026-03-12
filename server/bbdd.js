c101735d6277eafde1351bed39ed0e89a8e15c00
Edward Wayne Cruz <117868589+guaine86@users.noreply.github.com>
2026-03-12 11:55:02 -0300

    Update bbdd.js

--- server/bbdd.js
+++ server/bbdd.js
@@ -9,8 +9,8 @@ const conexion = mysql.createPool({
     password: process.env.DB_PASSWORD,
     port: 3306,
     waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    connectionLimit: 5,
+    maxIdle: 5, // max idle connections, the default value is the same as `connectionLimit`
     idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
     queueLimit: 0
 });
